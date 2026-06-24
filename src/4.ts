d67aa57c2789c77cac116737b97a6400c03ebedc
Ilona <ilona.gil@outlook.com>
2026-06-24 19:07:20 +0300

    add ts 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey() === this.key ? 'Person' : 'Someone'} entered the house.`);
+    } else {
+      console.log('The door is closed. Cannot come in.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door opened!');
+    } else {
+      console.log('Wrong key. Door remains closed.');
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
